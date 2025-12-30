4542252c133bd4f4aae2ba426a78ccc3c403d7c3
chesterzelaya <chesterzelaya@berkeley.edu>
2025-12-30 10:08:27 -0800

    discrete BFS search logic -- no duplicatioin logic

--- 06_OptimalControl/Pathplanning/discrete_feasible_planning.cpp
+++ 06_OptimalControl/Pathplanning/discrete_feasible_planning.cpp
@@ -1,1 +1,51 @@
 // by Chester Zelaya
+
+#include "discrete_feasible_planning.h"
+
+#include <queue>
+#include <vector>
+
+static bool stateVisited(const std::vector<Eigen::VectorXd> &visited,
+                         const Eigen::VectorXd &candidate) {
+  for (const auto &seen : visited) {
+    if (seen.isApprox(candidate)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool forwardSearch(const stateSpace::State &start,
+                   const stateSpace::State &goal, const stateSpace &S,
+                   const actionSpace &A) {
+  std::queue<Eigen::VectorXd> queue;
+  std::vector<Eigen::VectorXd> visited;
+
+  queue.push(start.x);
+  visited.push_back(start.x);
+
+  const Eigen::MatrixXd actions = A.getActions();
+
+  while (!queue.empty()) {
+    const Eigen::VectorXd current = queue.front();
+    queue.pop();
+
+    if (current.isApprox(goal.x)) {
+      return true;
+    }
+
+    for (int col = 0; col < actions.cols(); ++col) {
+      const Eigen::VectorXd action = actions.col(col);
+      const Eigen::VectorXd next = S.computeNextState(current, action);
+
+      if (!stateVisited(visited, next)) {
+        visited.push_back(next);
+        queue.push(next);
+      } else {
+        // Duplicate state -- solution not entirely specified rn
+      }
+    }
+  }
+
+  return false;
+}
--- 06_OptimalControl/Pathplanning/discrete_feasible_planning.h
+++ 06_OptimalControl/Pathplanning/discrete_feasible_planning.h
@@ -7,13 +7,34 @@
  * that are chosen by a planner. Each action u when applied to current
  * state x produces a new state x' */
 
+class stateSpace {
+public:
+  struct State {
+    Eigen::VectorXd x;
+    bool is_init;
+    bool is_goal;
+    bool is_seen;
+  };
+
+  Eigen::MatrixXd states_; // Collection of States
+  Eigen::MatrixXd getStates() const;
+  void addState(State X); // Add state, matrix of measurable
+                          // variables, can determine if a state is initial
+                          // state of goal state
+
+  Eigen::VectorXd computeNextState(Eigen::MatrixXd curr_state,
+                                   Eigen::MatrixXd actions) const;
+};
+
 class actionSpace {
 public:
-  Eigen::MatrixXd getActions(); // Call to get actions in actionspace
-  void addActions(Eigen::MatrixXd actions,
-                  Eigen::MatrixXd state); // Add actions based on state (not all
+  Eigen::MatrixXd actions_;
+  Eigen::MatrixXd getActions() const; // Call to get actions in actionspace
+  void addActions(Eigen::VectorXd actions,
+                  Eigen::VectorXd state); // Add actions based on state (not all
                                           // necessarily disjoint)
 };
 
-Eigen::MatrixXd computeState(Eigen::MatrixXd curr_state,
-                             Eigen::MatrixXd actions);
+bool forwardSearch(const stateSpace::State &start,
+                   const stateSpace::State &goal, const stateSpace &S,
+                   const actionSpace &A);
